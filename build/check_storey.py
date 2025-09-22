8fec09803ca2a295d2b58d9880caf03d0e753d4e
diegob1212 <diego.bruttin@epfl.ch>
2025-09-22 14:09:26 +0200

    Add files via upload

---
+++ build/check_storey.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 15 14:13:22 2025
+
+@author: Diego
+"""
+
+import ifcopenshell
+import ifcopenshell.util.classification
+
+model = ifcopenshell.open("25-06-D-STR.ifc")
+
+print('The report claims that DTU building 115 is a four-story structure that includes a basement and has a flat roof upon which is a terrace.\n')
+
+#Claim of the report
+report_number_storey = 6
+
+
+
+# Find all storeys
+storeys = model.by_type("IfcBuildingStorey")
+
+
+print("Number of storeys (IfcBuildingStorey):", len(storeys),'\n')
+
+# Print names and elevations of each storey in meters
+for s in storeys:
+    elevation_mm = getattr(s, "Elevation", 0)   
+    elevation_m = elevation_mm / 1000           
+    print(f" - Storey: {s.Name} | Elevation: {elevation_m} m \n")
+
+#Check/Assert validity of the claim 
+if report_number_storey is len(storeys):
+    print('Claim is correct ! DTU Building 115 has 6 storeys including terrace and basement.')
+    
+else:
+    print('Claim is incorrect ! In the report we have',report_number_storey,'storeys and the IFC model has', len(storeys))
